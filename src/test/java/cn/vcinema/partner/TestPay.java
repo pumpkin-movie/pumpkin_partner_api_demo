12b67fb415c44b6c0f0e25347f7cb72deab81fb0
Xulin Zhuang <zhuangxulin@vcinema.cn>
2018-01-30 17:47:00 +0800

    增加获取兑换码操作统计测试用例

--- src/test/java/cn/vcinema/partner/TestPay.java
+++ src/test/java/cn/vcinema/partner/TestPay.java
@@ -21,12 +21,14 @@
 package cn.vcinema.partner;
 
 import com.alibaba.fastjson.JSON;
+import org.apache.commons.collections.map.LinkedMap;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -54,6 +56,11 @@ public class TestPay {
     public void tearDown() throws Exception {
     }
 
+    /**
+     * 成功获取兑换码测试
+     *
+     * @throws Exception
+     */
     @Test
     public void getRedeemCodeSuccessful() throws Exception {
         String signatureNonce = Random.getRandom(10,Random.TYPE.LETTER_CAPITAL_NUMBER);
@@ -71,10 +78,17 @@ public class TestPay {
         parameter.add(new BasicNameValuePair("format", PartnerInfo.format));
         parameter.add(new BasicNameValuePair("version", PartnerInfo.version));
         parameter.add(new BasicNameValuePair("sign", PartnersApiSignature.partnersApiSignature(PartnerInfo.httpPostMethod,PartnerInfo.pay_action,PartnerInfo.format,PartnerInfo.pid,signatureNonce,PartnerInfo.accessSecret,timestamp,params)));
-        PayResponseBean result = JSON.parseObject(HttpClientUtil.doPost("http://dev.api.guoing.com:3505/pay/redeem_code",parameter),PayResponseBean.class);
+        PayResponseBean result = JSON.parseObject(HttpClientUtil.doPost("http://dev.api.guoing.com:3505"+PartnerInfo.pay_action,parameter),PayResponseBean.class);
         assertEquals("200",result.getStatusCode());
     }
 
+    /**
+     * 签名错误测试
+     *
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     * @throws UnsupportedEncodingException
+     */
     @Test
     public void getRedeemCodeSignFailure() throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
         String signatureNonce = Random.getRandom(10,Random.TYPE.LETTER_CAPITAL_NUMBER);
@@ -88,8 +102,61 @@ public class TestPay {
         parameter.add(new BasicNameValuePair("format", PartnerInfo.format));
         parameter.add(new BasicNameValuePair("version", PartnerInfo.version));
         parameter.add(new BasicNameValuePair("sign", "error sign info"));
-        PayResponseBean result = JSON.parseObject(HttpClientUtil.doPost("http://dev.api.guoing.com:3505/pay/redeem_code",parameter),PayResponseBean.class);
+        PayResponseBean result = JSON.parseObject(HttpClientUtil.doPost("http://dev.api.guoing.com:3505"+PartnerInfo.pay_action,parameter),PayResponseBean.class);
         assertEquals("17006",result.getStatusCode());
+    }
+
+    /**
+     * PID错误测试
+     *
+     * @throws UnsupportedEncodingException
+     * @throws InvalidKeyException
+     * @throws NoSuchAlgorithmException
+     */
+    @Test
+    public void getRedeemCodePidError() throws UnsupportedEncodingException, InvalidKeyException, NoSuchAlgorithmException {
+        String signatureNonce = Random.getRandom(10,Random.TYPE.LETTER_CAPITAL_NUMBER);
+        long timestamp = System.currentTimeMillis();
+
+        Map<String,String> params = new HashMap<>();
+        params.put("code_type",PartnerInfo.codeType);
+        params.put("version",PartnerInfo.version);
+
+        List<NameValuePair> parameter = new ArrayList<>();
+        parameter.add(new BasicNameValuePair("pid", "ERROR_PID"));
+        parameter.add(new BasicNameValuePair("code_type", PartnerInfo.codeType));
+        parameter.add(new BasicNameValuePair("timestamp", timestamp+""));
+        parameter.add(new BasicNameValuePair("signature_nonce", signatureNonce));
+        parameter.add(new BasicNameValuePair("format", PartnerInfo.format));
+        parameter.add(new BasicNameValuePair("version", PartnerInfo.version));
+        parameter.add(new BasicNameValuePair("sign", PartnersApiSignature.partnersApiSignature(PartnerInfo.httpPostMethod,PartnerInfo.pay_action,PartnerInfo.format,"ERROR_PID",signatureNonce,PartnerInfo.accessSecret,timestamp,params)));
+        PayResponseBean result = JSON.parseObject(HttpClientUtil.doPost("http://dev.api.guoing.com:3505"+PartnerInfo.pay_action,parameter),PayResponseBean.class);
+        assertEquals("17007",result.getStatusCode());
+    }
+
+    /**
+     * 获取兑换码统计成功测试
+     *
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     * @throws IOException
+     */
+    @Test
+    public void getPayStatisticsSuccessful() throws NoSuchAlgorithmException, InvalidKeyException, IOException {
+        String signatureNonce = Random.getRandom(10,Random.TYPE.LETTER_CAPITAL_NUMBER);
+        long timestamp = System.currentTimeMillis();
+        LinkedMap params = new LinkedMap();
+        params.put("version", PartnerInfo.version);
+        params.put("start_date","2018-01-01");
+        params.put("end_date","2018-01-30");
+        params.put("pid",PartnerInfo.pid);
+        params.put("timestamp", timestamp+"");
+        params.put("signature_nonce", signatureNonce);
+        params.put("format", PartnerInfo.format);
+        params.put("sign", PartnersApiSignature.partnersApiSignature(PartnerInfo.httpGetMethod,PartnerInfo.pay_statistics_action,PartnerInfo.format,PartnerInfo.pid,signatureNonce,PartnerInfo.accessSecret,timestamp,params));
+
+        PayStatisticsResponseBean result = JSON.parseObject(HttpClientUtil.doGet("http://dev.api.guoing.com:3505"+PartnerInfo.pay_statistics_action,params),PayStatisticsResponseBean.class);
+        assertEquals("200",result.getStatusCode());
     }
 
 
