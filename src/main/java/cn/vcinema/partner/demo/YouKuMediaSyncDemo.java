593097580f60bc0f4e3fcfffe9853759bf7ca296
MiriKuma <superkuma874@gmail.com>
2020-11-24 16:26:34 +0800

    update: update YouKu demo

--- src/main/java/cn/vcinema/partner/demo/YouKuMediaSyncDemo.java
+++ src/main/java/cn/vcinema/partner/demo/YouKuMediaSyncDemo.java
@@ -4,10 +4,6 @@ import cn.vcinema.partner.*;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.collections.map.LinkedMap;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
 
 /**
  * @author Han ZC
@@ -16,16 +12,12 @@ import org.junit.Test;
 public class YouKuMediaSyncDemo {
     private static final String PREFIX_URL = "https://dev-environmental.vcinema.cn:3505";
 
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-
-    @Test
-    public void getChangeListSuccessful() throws Exception {
+    /**
+     * 获取媒资内容变动列表
+     *
+     * @throws Exception
+     */
+    public static JSONObject getChangeListSuccessful() throws Exception {
         String signatureNonce = Random.getRandom(10, Random.TYPE.LETTER_CAPITAL_NUMBER);
         long timestamp = System.currentTimeMillis();
 
@@ -33,6 +25,7 @@ public class YouKuMediaSyncDemo {
         String accessSecret = PartnerInfo.accessSecret;
         String action = "/movie/change_list";
 
+        // 封装加密参数
         LinkedMap params = new LinkedMap();
         params.put("pid", pid);
         params.put("timestamp", Long.toString(timestamp));
@@ -40,13 +33,20 @@ public class YouKuMediaSyncDemo {
         params.put("format", PartnerInfo.format);
         params.put("version", PartnerInfo.version);
         params.put("sign", PartnersApiSignature.partnersApiSignature(PartnerInfo.httpGetMethod, action, PartnerInfo.format, pid, signatureNonce, accessSecret, timestamp, params));
-        JSONObject result = JSON.parseObject(HttpClientUtil.doGet(PREFIX_URL + action, params));
-        System.out.println(result);
-        Assert.assertEquals(200L, result.getLongValue("status_code"));
+        JSONObject responseJSON = JSON.parseObject(HttpClientUtil.doGet(PREFIX_URL + action, params));
+        if (200 == responseJSON.getInteger("status_code")) {
+            return responseJSON.getJSONObject("content");
+        } else {
+            return null;
+        }
     }
 
-    @Test
-    public void getRedirectLinkSuccessful() throws Exception {
+    /**
+     * 获取跳转链接
+     *
+     * @throws Exception
+     */
+    public static String getRedirectLinkSuccessful() throws Exception {
         String signatureNonce = Random.getRandom(10, Random.TYPE.LETTER_CAPITAL_NUMBER);
         long timestamp = System.currentTimeMillis();
 
@@ -58,13 +58,28 @@ public class YouKuMediaSyncDemo {
         params.put("movie_type", "1");
         params.put("movie_id", "30414");
         params.put("pid", pid);
-        params.put("timestamp",  Long.toString(timestamp));
+        params.put("timestamp", Long.toString(timestamp));
         params.put("signature_nonce", signatureNonce);
         params.put("format", PartnerInfo.format);
         params.put("version", PartnerInfo.version);
         params.put("sign", PartnersApiSignature.partnersApiSignature(PartnerInfo.httpGetMethod, action, PartnerInfo.format, pid, signatureNonce, accessSecret, timestamp, params));
-        JSONObject result = JSON.parseObject(HttpClientUtil.doGet(PREFIX_URL + action, params));
-        System.out.println(result);
-        Assert.assertEquals(200L, result.getLongValue("status_code"));
+        JSONObject responseJSON = JSON.parseObject(HttpClientUtil.doGet(PREFIX_URL + action, params));
+        if (200 == responseJSON.getInteger("status_code")) {
+            return responseJSON.getJSONObject("content").getString("full_url");
+        } else {
+            return null;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            JSONObject changelist = getChangeListSuccessful();
+            System.out.println(changelist);
+            String fullUrl = getRedirectLinkSuccessful();
+            System.out.println(fullUrl);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
